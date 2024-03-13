5fe3c35963ca9d362221683ece25a5657d1fa25e
AhmedSamy9828 <ahmed28samy98@gmail.com>
2024-03-13 05:20:16 +0200

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for c in range(122, 96, -1):
-    if c % 2 != 0:
-        c = c - 32
-    print("{}".format(chr(c)), end='')
+for x in range(122, 96, -1):
+    print("{}".format(chr(x) if x % 2 == 0 else chr(x - 32)), end="")
